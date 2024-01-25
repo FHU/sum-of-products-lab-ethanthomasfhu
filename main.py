89a8463a7249c1af2829ab5a2da6072042ffa9c8
Ethan Thomas <ethan.thomas@students.fhu.edu>
2024-01-25 15:48:32 -0600

    Update main.py
    Finished lab.

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-#REMOVE PASS AND FIX THE FUNCTION
-#change to test push and forks
-def sum_of_products(list1, list2):
-    pass
+list1 = (input())
+list2 = (input())
 
-if __name__ == '__main__':
-   #REMOVE PASS AND YOUR CODE GOES HERE
-    pass
+def sum_of_products(list1, list2):
+    sum = 0
+    for i in range(len(list1)):
+        sum += int(list1[i]) * int(list2[i])
+    return sum
+
+if list1 != list2:
+    print ("Error.")
+
+if len(list1) == len(list2):
+     print(sum_of_products(list1, list2))
