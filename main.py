abb10d3706528794bf016436ae0fee41d9e22484
Ethan Thomas <ethan.thomas@students.fhu.edu>
2024-01-25 15:53:23 -0600

    Update main.py
    Changed to length.

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def sum_of_products(list1, list2):
         sum += int(list1[i]) * int(list2[i])
     return sum
 
-if list1 != list2:
+if len(list1) != len(list2):
     print ("Error.")
 
 if len(list1) == len(list2):
